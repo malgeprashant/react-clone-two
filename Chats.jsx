d122905b20abcf598ae3eacd91b4c64606ac7681
malgeprashant <109685863+malgeprashant@users.noreply.github.com>
2022-12-22 15:23:50 +0530

    Add files via upload

---
+++ Chats.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Chat = () => {
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <div className="footer" style={{ position: "absolute" }}>
+          <h1
+            style={{
+              color: "white",
+              fontSize: "10px",
+              position: "relative",
+              top: "500px",
+              left: "30px",
+            }}
+          >
+            Powered by teamJeenyus
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
